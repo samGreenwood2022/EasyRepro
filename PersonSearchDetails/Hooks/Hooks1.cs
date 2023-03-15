fbebca9e1603e5a32e561545c409fcb4310f3497
samGreenwood <sam.greenwood@wales.nhs.uk>
2023-03-15 14:06:58 +0000

    Added comments and tidied up code

--- PersonSearchDetails/Hooks/Hooks1.cs
+++ PersonSearchDetails/Hooks/Hooks1.cs
@@ -12,25 +12,30 @@ namespace PersonSearchDetails.Hooks
     [Binding]
     public class SingleBrowserHook
     {
+        // initialise our fields so we can use them
         private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
         private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
         private readonly IObjectContainer _objectContainer;
 
         public SingleBrowserHook(IObjectContainer objectContainer)
         {
+            // our constructor creates an 'object container' where we can store things we want to use later
             _objectContainer = objectContainer;
         }
 
         [BeforeScenario]
         public void BeforeScenarioWithTag()
         {
+            // This before scenario initialises our browser before each test
             var xrmBrowser = new Microsoft.Dynamics365.UIAutomation.Api.Browser(TestSettings.Options);
             var driver = xrmBrowser.Driver;
             DHCWExtensions.Login(xrmBrowser, _username, _password);
             xrmBrowser.GuidedHelp.CloseGuidedHelp();
             driver.Manage().Window.Maximize();
 
-
+            // here we are storing an instance of IWebDriver and Browser
+            // IWebDriver allows us to control our Browser
+            // Browser contains Dynamics365 methods allowing us to interact the app, ie click certian option in the command bar
             _objectContainer.RegisterInstanceAs<IWebDriver>(driver);
             _objectContainer.RegisterInstanceAs<Browser>(xrmBrowser);
 
@@ -39,6 +44,7 @@ namespace PersonSearchDetails.Hooks
         [AfterScenario]
         public void AfterScenario()
         {
+            // This closes the browser down after the test is complete
             var webDriver = _objectContainer.Resolve<IWebDriver>();
 
             if (webDriver == null) return;
