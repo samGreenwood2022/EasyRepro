bdddc4e9f6312f76773764f282beb5ef4275e999
samGreenwood <sam.greenwood@wales.nhs.uk>
2023-03-16 13:18:52 +0000

    removing dependancies to other projects

---
+++ PersonSearchDetails/TestSettings.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using System;
+
+namespace WCCIS.Specs
+{
+    public static class TestSettings
+    {
+        public static string InvalidAccountLogicalName = "accounts";
+
+        public static string LookupField = "primarycontactid";
+        public static string LookupName = "Rene Valdes (sample)";
+        private static readonly string Type = System.Configuration.ConfigurationManager.AppSettings["BrowserType"].ToString();
+
+        public static BrowserOptions Options = new BrowserOptions
+        {
+            BrowserType = (BrowserType)Enum.Parse(typeof(BrowserType), Type),
+            PrivateMode = false,
+            FireEvents = false,
+            Headless = false,
+            UserAgent = false,
+            //DriversPath = @"""C:\Users\sa286848\Documents\RedCortex\EasyRepro\Microsoft.Dynamics365.UIAutomation.Sample\chromedriver\chromedriver.exe"""
+        };
+    }
+}
