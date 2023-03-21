f1bdc076f4033fbfc36010056545112cfcf9fe2d
samGreenwood <sam.greenwood@wales.nhs.uk>
2023-03-21 11:58:38 +0000

    Create the start of scenario 6

---
+++ PersonSearchDetails/Scripts/Feature1.feature
@@ -0,0 +1,9 @@
+﻿Feature: Login
+
+A simple feature to log us into CareDirector
+
+@tag1
+Scenario: CareDirector login
+	Given that i've logged in as an administrator
+	When [action]
+	Then [outcome]
--- PersonSearchDetails/StepDefinitions/PersonAmendStepDefinitions.cs
+++ PersonSearchDetails/StepDefinitions/PersonAmendStepDefinitions.cs
@@ -1,25 +1,30 @@
 using Microsoft.Dynamics365.UIAutomation.Api;
 using Microsoft.Dynamics365.UIAutomation.Browser;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Linq;
 using TechTalk.SpecFlow;
 using WCCIS.Specs.Extentions;
 
-namespace WCCIS.specs.StepDefinitions
+namespace PersonSearchDetails.StepDefinitions
 {
     [Binding]
-    public class PersonCreateStepDefinitions
+    public class PersonAmendStepDefinitions
     {
-
         private readonly IWebDriver driver;
         private readonly Browser xrmBrowser;
         public string lastname { get; set; }
         public string personId { get; set; }
 
-        public PersonCreateStepDefinitions(IWebDriver webDriver, Browser browser)//constructor
+        public string propertyNo = "137";
+        public string street = "Clydesdale Road";
+        public string townCity = "Newcastle Upon Tyne";
+        public string county = "Tyne and Wear";
+        public string postCode = "NE6 2EQ";
+        // public string dateMovedInNew = "01/01/2003";
+
+        public PersonAmendStepDefinitions(IWebDriver webDriver, Browser browser)//constructor
         {
             // Create an instance of our webbrowser (defined in the Hooks) to be used by all methods in this class
             // also create an instance of a dynamics365 library (xrmBrowser) containing commands we can use
@@ -27,30 +32,10 @@ namespace WCCIS.specs.StepDefinitions
             xrmBrowser = browser;
         }
 
-        [Given(@"that i've logged in as an administrator")]
-        public void GivenThatIveLoggedInAsAnAdministrator()
+        [Given(@"a known person already exists in the system (.*) and (.*) and (.*) and (.*) and (.*)")]
+        public void GivenAKnownPersonAlreadyExistsInTheSystem(string firstname, string dob, string dateMovedIn, string ethnicity, string gender)
         {
-            // removes any popups displayed when we 1st log in
-            try
-            {
-                driver.SwitchTo().Frame("InlineDialog_Iframe");
-                xrmBrowser.ThinkTime(2000);
-                driver.FindElement(By.XPath("//*[@id=\"butBegin\"]")).Click();
-               
-            }
-            catch 
-            {
-                Console.WriteLine("No popup displayed");
-
-            }
-               // check to ensure the caredirector logo is displayed
-            driver.FindElement(By.XPath("//*[@id=\"navTabLogoTextId\"]/img"));
-        }
-
-
-        [When(@"a person is created by completing mandatory fields only (.*) and (.*) and (.*) and (.*) and (.*)")]
-        public void WhenAPersonIsCreatedByCompletingMandatoryFieldsOnly(string firstname, string dob, string dateMovedIn, string ethnicity, string gender)
-        {
+            // Create a new person - can we call the  method 'When a person is created by completing mandatory fields only'
             xrmBrowser.Navigation.OpenSubArea("Workplace", "People");
             xrmBrowser.CommandBar.ClickCommand("NEW PERSON");
             driver.SwitchTo().Window(driver.WindowHandles.Last());
@@ -60,14 +45,15 @@ namespace WCCIS.specs.StepDefinitions
             driver.FindElement(By.XPath("//*[@id=\"firstname\"]/div[1]")).Click();
             driver.FindElement(By.XPath("//*[@id=\"firstname_i\"]")).SendKeys(firstname);
             driver.FindElement(By.XPath("//*[@id=\"lastname\"]/div[1]")).Click();
-            // generate a random string for surname, false or true sets the string to upper or lower case
+            // generate a random string for surname
             lastname = DHCWExtensions.RandomString(6, false);
             driver.FindElement(By.Id("lastname_i")).SendKeys(lastname);
             driver.FindElement(By.XPath("//*[@id=\"cw_ethnicityid\"]/div[1]")).Click();
             driver.FindElement(By.XPath("//*[@id=\"cw_ethnicityid_ledit\"]")).SendKeys(ethnicity);
             xrmBrowser.ThinkTime(1000);
             // Select the first value from the gender picklist
-            driver.FindElement(By.XPath("//*[@id=\"gendercode\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"gendercode_cl\"]")).Click();
+
             var dropDownOption = driver.FindElement(By.XPath("//*[@id=\"gendercode_i\"]"));
             var selectElement = new SelectElement(dropDownOption);
             selectElement.SelectByText(gender);
@@ -80,45 +66,54 @@ namespace WCCIS.specs.StepDefinitions
             driver.FindElement(By.XPath("//*[@id=\"Date Person moved in_label\"]")).Click();
             driver.FindElement(By.XPath("//*[@id=\"cw_datepersonmovedin_iDateInput\"]")).SendKeys(dateMovedIn);
             xrmBrowser.ThinkTime(1000);
+            // add an address (currently hard coded above)
+            DHCWExtensions.enterAddressDetails(xrmBrowser, driver, propertyNo, street, townCity, county, postCode);
+            
+            //driver.FindElement(By.XPath("//*[@id=\"cw_propertyno_cl\"]")).Click();
+            //driver.FindElement(By.XPath("//*[@id=\"cw_propertyno_i\"]")).SendKeys(propertyNo);
+            //xrmBrowser.ThinkTime(1000);
+            //driver.FindElement(By.XPath("//*[@id=\"address1_line1_cl_span\"]")).Click();
+            //driver.FindElement(By.XPath("//*[@id=\"address1_line1_i\"]")).SendKeys(firstLineOfAddress);
+            //xrmBrowser.ThinkTime(1000);
+            //driver.FindElement(By.XPath("//*[@id=\"address1_postalcode_cl\"]")).Click();
+            //driver.FindElement(By.XPath("//*[@id=\"address1_postalcode_i\"]")).SendKeys(postCode);
+            // click postcode lookup
+            //driver.FindElement(By.XPath("//*[@id=\"address1_postalcodeAddressSearch\"]")).Click();
+            
+            xrmBrowser.ThinkTime(1000);
             // save the record
-            xrmBrowser.CommandBar.ClickCommand("SAVE & CLOSE");
+            // xrmBrowser.CommandBar.ClickCommand("SAVE & CLOSE");
             xrmBrowser.ThinkTime(3000);
             Console.WriteLine(lastname);
-             
+
+            // search for our person, the search person method should be called from here
+            DHCWExtensions.personSearch(xrmBrowser, driver, firstname, lastname, dob);
         }
 
-        [Then(@"new person can be returned in a search (.*) and (.*)")]
-        public void ThenNewPersonCanBeReturnedInASearch(string firstname, string dob)
+        [When(@"i amend a persons primary address details (.*) and (.*) and (.*) and (.*) and (.*)")]
+        public void WhenIAmendAPersonsPrimaryAddressDetails(string propertyNo, string street, string townCity, string county, string postcode)
         {
-            // search for our person
-            xrmBrowser.CommandBar.ClickCommand("PERSON SEARCH");
-            driver.SwitchTo().Window(driver.WindowHandles.Last());
-            xrmBrowser.ThinkTime(1000);
-            driver.FindElement(By.XPath("//*[@id=\"txtFirstName\"]")).SendKeys(firstname);
-            xrmBrowser.ThinkTime(1000);
-            driver.FindElement(By.Name("txtLastName")).SendKeys(lastname);
-            xrmBrowser.ThinkTime(1000);
-            driver.FindElement(By.Name("txtDOB")).SendKeys(dob);
-            xrmBrowser.ThinkTime(1000);
-            driver.FindElement(By.Name("btnFind")).Click();
             xrmBrowser.ThinkTime(2000);
-
-
-            // finds the element that stores the person id by searching on a partial id
-            // then getting the text value from that element
-            String personId = driver.FindElement(By.XPath("//*[contains(@id, 'cw_clientid')]")).Text;
-            Console.WriteLine(personId);           
-            // the Actions class contains functions like 'doubleClick' which can be used on ui elements
-            Actions act = new Actions(driver);
-            IWebElement row = driver.FindElement(By.XPath("//*[text()[contains(.,'" + firstname + "')]]"));
-            act.DoubleClick(row).Perform();
+            driver.FindElement(By.Id("FormSecNavigationControl-Icon")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"flyoutFormSection_Cell\"]")).Click();
             xrmBrowser.ThinkTime(2000);
+            DHCWExtensions.enterAddressDetails(xrmBrowser, driver, propertyNo, street, townCity, county, postcode);
+            xrmBrowser.ThinkTime(1000);
+            // xrmBrowser.CommandBar.ClickCommand("SAVE");
+            xrmBrowser.ThinkTime(6000);
+        }
+
+        [Then(@"Then the new address will replace the old address on the persons record (.*) and (.*)")]
+        public void ThenTheNewAddressWillReplaceTheOldAddressOnThePersonsRecord(string firstname, string dob)
+        {
+            // call our personSearch method
+            DHCWExtensions.personSearch(xrmBrowser, driver, firstname, lastname, dob);
+            
             // switch tio the correct browser window and iFrame we want to use
             driver.SwitchTo().Window(driver.WindowHandles.First());
             driver.SwitchTo().Window(driver.WindowHandles[2]);
             driver.SwitchTo().Frame("contentIFrame0");
             driver.SwitchTo().Frame(driver.FindElement(By.Id("IFRAME_Banner")));
-
             // verify our lastname, firstname and id is correct, then store in a string so we can see what it is
             String concatName = driver.FindElement(By.XPath("//*[text()='" + lastname + ", " + firstname + " (WCCIS ID: " + personId + ")']")).Text;
             // write the string to the console so we can see whats in it, handy for debugging
@@ -127,8 +122,6 @@ namespace WCCIS.specs.StepDefinitions
             driver.FindElement(By.XPath("//*[text()='" + lastname + ", " + firstname + " (WCCIS ID: " + personId + ")']"));
             //search for our dob value within the iframe
             driver.FindElement(By.XPath("//*[text()[contains(.,'" + dob + "')]]"));
-
         }
-        
     }
 }
