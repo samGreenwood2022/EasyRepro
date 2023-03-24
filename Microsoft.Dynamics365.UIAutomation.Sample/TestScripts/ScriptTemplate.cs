d065e4a3dd8b864bc3de1b03202b2477d70ef718
samGreenwood <sam.greenwood@wales.nhs.uk>
2023-03-24 11:02:50 +0000

    Created Script Template

---
+++ Microsoft.Dynamics365.UIAutomation.Sample/TestScripts/ScriptTemplate.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using Microsoft.Dynamics365.UIAutomation.Sample.Extentions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System.Linq;
+using System.Security;
+
+namespace Microsoft.Dynamics365.UIAutomation.Sample.Web
+{
+    [TestClass]
+    public class ScriptTemplate
+    {
+
+        private readonly SecureString _username = System.Configuration.ConfigurationManager.AppSettings["OnlineUsername"].ToSecureString();
+        private readonly SecureString _password = System.Configuration.ConfigurationManager.AppSettings["OnlinePassword"].ToSecureString();
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            using (var xrmBrowser = new Api.Browser(TestSettings.Options))
+            {
+                var driver = xrmBrowser.Driver;
+                DHCWExtentions.Login(xrmBrowser, _username, _password);
+                xrmBrowser.GuidedHelp.CloseGuidedHelp();
+                driver.Manage().Window.Maximize();
+                xrmBrowser.ThinkTime(2000);
+                
+
+
+
+
+            }
+        }
+    }
+}
--- PersonSearchDetails/Scripts/DemoPersonSearch.feature
+++
@@ -1,9 +0,0 @@
-﻿Feature: Person Search & Verify Record
-
-Perform a person search and verify the record
-
-@tag1
-Scenario: Perform a person search
-	Given that i've logged in as a Test Administrator
-	When when i search for a person
-	Then the expected record will be displayed on screen
