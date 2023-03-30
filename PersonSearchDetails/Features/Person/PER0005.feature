7304432adf52bfd11bb7187c63ea0367c55ef79d
samGreenwood <sam.greenwood@wales.nhs.uk>
2023-03-30 13:59:18 +0100

    completed the scenario

---
+++ PersonSearchDetails/Features/Person/PER0005.feature
@@ -0,0 +1,13 @@
+﻿Feature: Ensure duplicate detection rules are working
+
+To verify that the duplicate detection rules are in place when creating a new Person
+Detection rule tested - matching Surname, Forename & DOB
+
+@tag1
+Scenario Outline: Verify that the duplicate detection rules are in place when creating a new Person
+	Given that i've logged in as an administrator
+	When i create two people using the same details <firstname> and <dob> and <dateMovedIn> and <Ethnicity> and <Gender> and <preferredLanguage>
+	Then the duplicate detection rules will trigger
+	Examples: 
+	| firstname | dob        | dateMovedIn | Ethnicity | Gender | preferredLanguage |
+	| John      | 12/08/1976 | 01/01/2000  | African   | Male   | Welsh             |
--- PersonSearchDetails/StepDefinitions/PersonCreateStepDefinitions.cs
+++ PersonSearchDetails/StepDefinitions/PersonCreateStepDefinitions.cs
@@ -160,8 +160,99 @@ namespace WCCIS.specs.StepDefinitions
             Assert.AreEqual(shortDate, todaysDate);
             xrmBrowser.ThinkTime(2000);
 
-
         }
+
+        [When(@"i create two people using the same details (.*) and (.*) and (.*) and (.*) and (.*) and (.*)")]
+        public void WhenICreateTwoPeopleUsingTheSameDetailsForenameAndAndAndAfricanAndMaleAndWelsh(string firstname, string dob, string dateMovedIn, string ethnicity, string gender, string preferredLanguage)
+        {
+            xrmBrowser.Navigation.OpenSubArea("Workplace", "People");
+            xrmBrowser.CommandBar.ClickCommand("NEW PERSON");
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            xrmBrowser.ThinkTime(1000);
+            // select the correct iFrame
+            driver.SwitchTo().Frame("contentIFrame1");
+            xrmBrowser.ThinkTime(1000);
+            driver.FindElement(By.XPath("//*[@id=\"firstname\"]/div[1]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"firstname_i\"]")).SendKeys(firstname);
+            driver.FindElement(By.XPath("//*[@id=\"lastname\"]/div[1]")).Click();
+            // generate a random string for surname, false or true sets the string to upper or lower case
+            lastname = DHCWExtensions.RandomString(6, false);
+            driver.FindElement(By.Id("lastname_i")).SendKeys(lastname);
+            driver.FindElement(By.XPath("//*[@id=\"cw_ethnicityid\"]/div[1]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"cw_ethnicityid_ledit\"]")).SendKeys(ethnicity);
+            xrmBrowser.ThinkTime(1000);
+            // enter value into preferred language field
+            driver.FindElement(By.XPath("//*[@id=\"cw_languageid_cl\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"cw_languageid_ledit\"]")).SendKeys(preferredLanguage);
+            // Select the first value from the gender picklist
+            driver.FindElement(By.XPath("//*[@id=\"gendercode\"]")).Click();
+            var dropDownOption = driver.FindElement(By.XPath("//*[@id=\"gendercode_i\"]"));
+            var selectElement = new SelectElement(dropDownOption);
+            selectElement.SelectByText(gender);
+            //selectElement.SelectByIndex(0);
+            xrmBrowser.ThinkTime(1000);
+            // enter a value into the dob field
+            driver.FindElement(By.XPath("//*[@id=\"Date of Birth_label\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"birthdate_iDateInput\"]")).SendKeys(dob);
+            xrmBrowser.ThinkTime(2000);
+            driver.FindElement(By.XPath("//*[@id=\"Date Person moved in_label\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"cw_datepersonmovedin_iDateInput\"]")).SendKeys(dateMovedIn);
+            xrmBrowser.ThinkTime(1000);
+            // save the record
+            xrmBrowser.CommandBar.ClickCommand("SAVE");
+            xrmBrowser.ThinkTime(3000);
+            Console.WriteLine(lastname);
+
+            // create the 2nd person using the same details as above
+            // for info the create person steps could be turned into one re-usable method to reduce duplicated code
+            xrmBrowser.Navigation.OpenSubArea("Workplace", "People");
+            xrmBrowser.CommandBar.ClickCommand("NEW PERSON");
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            xrmBrowser.ThinkTime(1000);
+            // select the correct iFrame
+            driver.SwitchTo().Frame("contentIFrame1");
+            xrmBrowser.ThinkTime(1000);
+            driver.FindElement(By.XPath("//*[@id=\"firstname\"]/div[1]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"firstname_i\"]")).SendKeys(firstname);
+            driver.FindElement(By.XPath("//*[@id=\"lastname\"]/div[1]")).Click();
+            // use the same details tat were entered above
+            driver.FindElement(By.Id("lastname_i")).SendKeys(lastname);
+            driver.FindElement(By.XPath("//*[@id=\"cw_ethnicityid\"]/div[1]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"cw_ethnicityid_ledit\"]")).SendKeys(ethnicity);
+            xrmBrowser.ThinkTime(1000);
+            // enter value into preferred language field
+            driver.FindElement(By.XPath("//*[@id=\"cw_languageid_cl\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"cw_languageid_ledit\"]")).SendKeys(preferredLanguage);
+
+            // Select the first value from the gender picklist
+            driver.FindElement(By.XPath("//*[@id=\"gendercode\"]")).Click();
+            var dropDownOption2 = driver.FindElement(By.XPath("//*[@id=\"gendercode_i\"]"));
+            var selectElement2 = new SelectElement(dropDownOption2);
+            selectElement2.SelectByText(gender);
+            //selectElement.SelectByIndex(0);
+            xrmBrowser.ThinkTime(1000);
+            // enter a value into the dob field
+            driver.FindElement(By.XPath("//*[@id=\"Date of Birth_label\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"birthdate_iDateInput\"]")).SendKeys(dob);
+            xrmBrowser.ThinkTime(2000);
+            driver.FindElement(By.XPath("//*[@id=\"Date Person moved in_label\"]")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"cw_datepersonmovedin_iDateInput\"]")).SendKeys(dateMovedIn);
+            xrmBrowser.ThinkTime(1000);
+            // save the record
+            xrmBrowser.CommandBar.ClickCommand("SAVE");
+            xrmBrowser.ThinkTime(3000);
+            Console.WriteLine(lastname);
+        }
+
+
+        [Then(@"the duplicate detection rules will trigger")]
+        public void ThenTheDuplicateDetectionRulesWillTrigger()
+        {
+            // upon writing this script the duplicate detection rules werent in place
+            driver.FindElement(By.XPath("//*[@id=\"Duplicate detect rules popup\"]")).Click();
+            Console.WriteLine("Duplicate detection rules currently not in place");
+        }
+
 
     }
 }
